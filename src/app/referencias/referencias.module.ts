44419d009c5a63759d96226b7145dc9462c2f17c
alexishimarumillon <alexishimarumillon@gmail.com>
2020-07-06 23:53:53 -0300

    criacao de modulo e componente referencias

---
+++ src/app/referencias/referencias.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReferenciasComponent } from './referencias.component';
+
+@NgModule({
+  declarations: [
+    ReferenciasComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ReferenciasComponent
+   ]
+})
+export class ReferenciasModule { }
